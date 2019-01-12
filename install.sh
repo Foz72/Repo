eaf8ffd5e4fb6ed2be6516c52e925233f1972054
Foz72 <46563422+Foz72@users.noreply.github.com>
2019-01-12 13:13:25 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -87,6 +87,11 @@ apt-get -y install unzip >> $LOG_FILE 2>&1
 apt-get -y install virtualenv >> $LOG_FILE 2>&1
 apt-get -y install python-virtualenv >> $LOG_FILE 2>&1
 apt-get -y install pwgen >> $LOG_FILE 2>&1
+apt-get -y install libzmq3-dev >> $LOG_FILE 2>&1
+apt-get -y install libboost1.58-all-dev >> $LOG_FILE 2>&1
+apt-get -y install libminiupnpc-dev >> $LOG_FILE 2>&1
+apt-get -y install libdb4.8-dev libdb4.8++-dev >> $LOG_FILE 2>&1
+apt-get -y install libevent-pthreads-2.0-5 >> $LOG_FILE 2>&1
 
 if [[ ("$install_fail2ban" == "y" || "$install_fail2ban" == "Y" || "$install_fail2ban" == "") ]]; then
 	decho "Optional installs : fail2ban"
@@ -113,6 +118,7 @@ decho "Create user $whoami (if necessary)"
 #desactivate trap only for this command
 trap '' ERR
 getent passwd $whoami > /dev/null 2&>1
+usermod -aG sudo $whoami
 
 if [ $? -ne 0 ]; then
 	trap 'error ${LINENO}' ERR
@@ -140,22 +146,47 @@ maxconnections=24
 masternode=1
 masternodeprivkey=$key
 externalip=$ip
+addnode=94.177.187.150
+addnode=118.27.35.22
+addnode=45.63.23.222
+addnode=139.99.172.53
+addnode=37.46.134.162
+addnode=167.160.185.68
+addnode=149.28.123.234
+addnode=51.15.242.133
+addnode=45.76.90.116
+addnode=85.198.140.75
+addnode=155.94.174.13
+addnode=192.161.176.95
+addnode=194.135.91.107
+addnode=199.247.0.118
+addnode=45.32.205.105
+addnode=123.111.141.215
+addnode=176.31.106.35
+addnode=45.63.90.77
+addnode=45.77.85.207
+addnode=45.32.252.55
+addnode=139.99.196.171
+addnode=209.250.230.219
+addnode=85.198.140.70
+addnode=51.15.101.105
+addnode=51.15.82.14
 EOF
 chown -R $whoami:$whoami /home/$whoami
 
 ## Download and Install new bin
-decho "Downloading new core and installing it"
+echo "Downloading new core and installing it"
 wget https://github.com/monacocoin-net/monoeci-core/releases/download/v0.12.2.3/monoeciCore-0.12.2.3-linux64.tar.gz >> $LOG_FILE 2>&1
 sudo tar xvf monoeciCore-0.12.2.3-linux64.tar.gz >> $LOG_FILE 2>&1
-sudo cp monoeciCore-0.12.2/bin/monoecid /usr/bin/ >> $LOG_FILE 2>&1
-sudo cp monoeciCore-0.12.2/bin/monoeci-cli /usr/bin/ >> $LOG_FILE 2>&1
-sudo cp monoeciCore-0.12.2/bin/monoeci-tx /usr/bin/ >> $LOG_FILE 2>&1
-rm -rf monoeciCore-0.12.2 >> $LOG_FILE 2>&1
+sudo cp monoecid /usr/bin/ >> $LOG_FILE 2>&1
+sudo cp monoeci-cli /usr/bin/ >> $LOG_FILE 2>&1
+sudo cp monoeci-tx /usr/bin/ >> $LOG_FILE 2>&1
+#rm -rf monoeciCore-0.12.2 >> $LOG_FILE 2>&1
 
 #Run monoecid as selected user
 sudo -H -u $whoami bash -c 'monoecid' >> $LOG_FILE 2>&1
 
-echo 'Monoeci Core prepared and lunched'
+echo 'Monoeci Core prepared and launched'
 
 sleep 10
 
@@ -163,7 +194,7 @@ sleep 10
 
 decho "Setting up sentinel"
 
-echo 'Downloading sentinel...'
+decho 'Downloading sentinel...'
 #Install Sentinel
 git clone https://github.com/monacocoin-net/sentinel.git /home/$whoami/sentinel >> $LOG_FILE 2>&1
 chown -R $whoami:$whoami /home/$whoami/sentinel >> $LOG_FILE 2>&1
@@ -199,19 +230,4 @@ echo "3- Once completed, please return to VPS and wait for the wallet to be sync
 echo "4- Then you can try the command 'monoeci-cli masternode status' to get the masternode status."
 
 su $whoami
-
-    © 2019 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Status
-    Help
-
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
-
-Press h to open a hovercard with more details.
+cd
